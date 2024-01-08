463359098c43b56d18300f4dced0a0fcee424ef4
Ebubejisos <anosikegideon007@gmail.com>
2024-01-08 17:25:36 +0100

    included reference links to downloaded icons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,24 @@
 const Footer = () => {
-	return <footer>Footer</footer>;
+	return (
+		<footer>
+			<div>footer</div>
+			<div>
+				<a target='_blank' href='https://icons8.com/icon/108784/javascript'>
+					JavaScript
+				</a>{' '}
+				icon by{' '}
+				<a target='_blank' href='https://icons8.com'>
+					Icons8
+				</a>
+				<a href='https://iconscout.com/icons/mongodb' target='_blank'>
+					Mongodb
+				</a>{' '}
+				by{' '}
+				<a href='https://iconscout.com/contributors/icon-mafia'>Icon Mafia</a>{' '}
+				on <a href='https://iconscout.com'>IconScout</a>
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
