11d229437994281a7f5e1dc5a3b6d135affb348c
Ebubejisos <anosikegideon007@gmail.com>
2024-01-15 02:26:37 +0100

    created a mini data file for rendering lists

---
+++ src/utils/projects.ts
@@ -0,0 +1,43 @@
+type Project = {
+  name?: string;
+  description?: string;
+  repoLink?: string;
+  liveLink?: string;
+  imgSrc?: string;
+  id: number;
+}
+
+export const projects: Project[] = [
+  {
+    name: 'Promptopia',
+    description: 'A next app with typescript, mongodb, with nextauth google login and credetials log in and more :)',
+    repoLink: 'https://github.com/Ebubejisos/Promptopia',
+    liveLink: 'https://next-js-promptopia-two.vercel.app/',
+    imgSrc: 'src/assets/images/stones.jpg',
+    id: 1,
+  },
+  {
+    name: 'CV-Builder',
+    description: 'CV-Builder helps you create your cv after filling in your necessary information and can be downloaded as a pdf. Built with react and inspired by https://www.theodinproject.com/',
+    repoLink: 'https://github.com/Ebubejisos/CV-React-app',
+    liveLink: 'https://ebubejisos.github.io/CV-React-app/',
+    imgSrc: '',
+    id: 2,
+  },
+  {
+    name: 'Krypt',
+    description: ' Krypt is my first web3.0 app which allows transfer of sepolia test ETH between users, login with metamask and start transferring test ETH',
+    repoLink: 'https://github.com/Ebubejisos/Krypt',
+    liveLink: 'https://web3-0-krypt.vercel.app/',
+    imgSrc: '',
+    id: 3,
+  },
+  {
+    name: 'Color Memory',
+    description: 'How well do you memorize? Play a simple game of color memory and avoid picking a color more than once and avoid running out of time. Built on react and inspired by https://www.theodinproject.com/',
+    repoLink: 'https://github.com/Ebubejisos/React-starter/tree/memoryCard',
+    liveLink: 'https://ebubejisos.github.io/React-starter/',
+    imgSrc: '',
+    id: 4,
+  },
+]
---
+++ src/utils/skills.ts
@@ -0,0 +1,45 @@
+interface Skill {
+  name?: string;
+  level?: string | number;
+  id: number | string;
+  logoSrc?: string;
+}
+
+export const codingSkills: Skill[] = [
+  {
+    name: 'Javascript',
+    level: '90%',
+    logoSrc: 'src/assets/icons/icons8-javascript.svg',
+    id: 1,
+  },
+  {
+    name: 'Cascading Style Sheets, CSS',
+    level: '93%',
+    logoSrc: 'src/assets/icons/icons8-css3.svg',
+    id: 2,
+  },
+  {
+    name: 'React',
+    level: '89%',
+    logoSrc: 'src/assets/icons/icons8-react-native.svg',
+    id: 3,
+  },
+  {
+    name: 'Typescript',
+    level: '84%',
+    logoSrc: 'src/assets/icons/icons8-typescript.svg',
+    id: 4,
+  },
+  {
+    name: 'NextJs',
+    level: '76%',
+    logoSrc: 'src/assets/icons/icons8-next.js.svg',
+    id: 5,
+  },
+  {
+    name: 'MongoDB',
+    level: '72%',
+    logoSrc: 'src/assets/icons/mongodb.svg',
+    id: 6,
+  },
+];
