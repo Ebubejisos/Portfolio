bf9f96d976818a957acb62aca5c32d374be41282
Ebubejisos <anosikegideon007@gmail.com>
2024-01-15 02:29:35 +0100

    built the portfolio section

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,5 +1,32 @@
+import { projects } from '../utils/projects';
+import '../assets/styles/portfolio.scss';
 const Portfolio = () => {
-	return <div id='portfolio'>Portfolio</div>;
+	return (
+		<section id='portfolio' className='portfolio-section'>
+			<h1>Portfolio</h1>
+			<div className='projects-container'>
+				{projects.map((project) => (
+					<div className='project' key={project.id}>
+						<div className='project-details'>
+							<h3>{project.name}</h3>
+							<p>{project.description}</p>
+							<button className='repo-link'>
+								<a href={project.repoLink}>github-repo</a>
+							</button>
+							<button className='live-link'>
+								<a href={project.liveLink}>live app</a>
+							</button>
+						</div>
+						<div className='img-container'>
+							<div className='img-frame'>
+								<img src={project.imgSrc} alt={project.name} />
+							</div>
+						</div>
+					</div>
+				))}
+			</div>
+		</section>
+	);
 };
 
 export default Portfolio;
