712734329bdb8673167da0c2eb23c0094780d4ae
Ebubejisos <anosikegideon007@gmail.com>
2024-01-18 19:59:24 +0100

    changed icon images to svgs in nav bar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,28 +1,33 @@
+/// <reference types="vite-plugin-svgr/client" />
+import HomeIcon from '../assets/icons/home.svg?react';
+import AboutIcon from '../assets/icons/account-circle.svg?react';
+import PortfolioIcon from '../assets/icons/dots-grid.svg?react';
+import ContactIcon from '../assets/icons/email.svg?react';
 const Nav = () => {
 	return (
 		<nav className='nav-bar'>
 			<ul className='nav-list'>
 				<li>
 					<a href='#'>
-						<img src='./src/assets/icons/home.svg' alt='' />
+						<HomeIcon />
 						<span>Home</span>
 					</a>
 				</li>
 				<li>
 					<a href='#about'>
-						<img src='./src/assets/icons/account-circle.svg' alt='' />
+						<AboutIcon />
 						<span>About</span>
 					</a>
 				</li>
 				<li>
 					<a href='#portfolio'>
-						<img src='./src/assets/icons/dots-grid.svg' alt='' />
+						<PortfolioIcon />
 						<span>Portfolio</span>
 					</a>
 				</li>
 				<li>
 					<a href='#contact'>
-						<img src='./src/assets/icons/email.svg' alt='' />
+						<ContactIcon />
 						<span>Contact</span>
 					</a>
 				</li>
