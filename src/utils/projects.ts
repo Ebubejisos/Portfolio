444b5bedb7417e5d11f5e8e70e4daedcc4261a88
Ebubejisos <anosikegideon007@gmail.com>
2026-04-24 20:09:42 +0100

    added each project image

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -1,4 +1,7 @@
-import image from '../../assets/images/stones.jpg';
+import inventory from '../../assets/images/Drug-inventory.png';
+import promptopia from '../../assets/images/Promptopia.png';
+import cvBuilder from '../../assets/images/CV-creator.png';
+import krypt from '../../assets/images/Krypt.png';
 
 type Project = {
   name?: string;
@@ -15,7 +18,7 @@ export const projects: Project[] = [
     description: 'Manage your inventory with ease, add items, update stock levels, and track your inventory in real-time. Built with nextjs, typescript, postgres and express',
     repoLink: 'https://github.com/Ebubejisos/Inventory-Tracker',
     liveLink: 'https://inventory-tracker-gold-two.vercel.app/',
-    imgSrc: image,
+    imgSrc: inventory,
     id: 0,
   },
   {
@@ -23,7 +26,7 @@ export const projects: Project[] = [
     description: 'A next app with typescript, mongodb, with nextauth google login and credetials log in and more :)',
     repoLink: 'https://github.com/Ebubejisos/Promptopia',
     liveLink: 'https://next-js-promptopia-two.vercel.app/',
-    imgSrc: image,
+    imgSrc: promptopia,
     id: 1,
   },
   {
@@ -31,7 +34,7 @@ export const projects: Project[] = [
     description: 'CV-Builder helps you create your cv after filling in your necessary information and can be downloaded as a pdf. Built with react and inspired by https://www.theodinproject.com/',
     repoLink: 'https://github.com/Ebubejisos/CV-React-app',
     liveLink: 'https://ebubejisos.github.io/CV-React-app/',
-    imgSrc: image,
+    imgSrc: cvBuilder,
     id: 2,
   },
   {
@@ -39,15 +42,7 @@ export const projects: Project[] = [
     description: ' Krypt is my first web3.0 app which allows transfer of sepolia test ETH between users, login with metamask and start transferring test ETH',
     repoLink: 'https://github.com/Ebubejisos/Krypt',
     liveLink: 'https://web3-0-krypt.vercel.app/',
-    imgSrc: image,
+    imgSrc: krypt,
     id: 3,
-  },
-  {
-    name: 'Color Memory',
-    description: 'How well do you memorize? Play a simple game of color memory and avoid picking a color more than once and avoid running out of time. Built on react and inspired by https://www.theodinproject.com/',
-    repoLink: 'https://github.com/Ebubejisos/React-starter/tree/memoryCard',
-    liveLink: 'https://ebubejisos.github.io/React-starter/',
-    imgSrc: image,
-    id: 4,
-  },
+  }
 ]
